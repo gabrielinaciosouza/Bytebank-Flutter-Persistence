4e6ebf259f534dc81c791c564c407baac7521052
Gabriel Inacio <gabriel.sert9@gmail.com>
2020-06-13 10:45:59 -0300

    Projeto concluido, bytebank com persistencia de dados

---
+++ lib/screens/contacts_form.dart
@@ -0,0 +1,60 @@
+import 'package:bytebank/dao/contact_dao.dart';
+import 'package:bytebank/models/contact.dart';
+import 'package:flutter/material.dart';
+
+class ContactsForm extends StatefulWidget {
+  @override
+  _ContactsFormState createState() => _ContactsFormState();
+}
+
+class _ContactsFormState extends State<ContactsForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController =
+      TextEditingController();
+  final ContactDao _dao = ContactDao();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Contact'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Full Name'),
+              style: TextStyle(fontSize: 24.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _accountNumberController,
+                decoration: InputDecoration(labelText: 'Account Number'),
+                style: TextStyle(fontSize: 24.0),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  child: Text('Create'),
+                  onPressed: () {
+                    String name = _nameController.text;
+                    int account = int.tryParse(_accountNumberController.text);
+                    final Contact newContact = Contact(0, name, account);
+                    _dao.save(newContact).then((id) => Navigator.pop(context));
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
